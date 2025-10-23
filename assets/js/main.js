788c9854effdf61f2b57f1940d9e6e2e38a03fdf
calogerosorce <calogerosorce00@hotmail.com>
2025-10-23 13:24:52 +0200

    Add initial settings for live server and update main.js with API usage instructions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+
+*/
+
+
 const url = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+function randomEmail(response) {
+    for (i = 0; i < 10; i++) {
+        console.log(response);
+    }
+}
+
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(res => res.json())
+    .then(data => {
+        const { success, response } = data
+        console.log(success, response);
+
+    })
